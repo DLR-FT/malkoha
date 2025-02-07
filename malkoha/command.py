cd78ae742c06dea94ac05df63ba597752a3e83a9
Tim Schubert <dadada@dadada.li>
2025-02-07 10:16:36 +0100

    refactor: put cli into separate module

---
+++ malkoha/command.py
@@ -0,0 +1,56 @@
+"""Malkoha CLI"""
+
+import sys
+
+
+from argparse import ArgumentParser, Namespace
+from xml.etree import ElementTree as ET
+
+from malkoha.extract import get_traces
+
+
+def run():
+    """Runs command"""
+
+    parser = ArgumentParser()
+    parser.add_argument("path", nargs="?", help="path to process", default=".")
+    parser.set_defaults(func=traces_cmd)
+
+    args = parser.parse_args()
+    if "func" in args:
+        args.func(args)
+    else:
+        parser.print_help()
+        sys.exit(1)
+
+
+def traces_cmd(args: Namespace):
+    """Writes traces to XML file"""
+
+    traces = ET.Element("traces")
+    for trace in get_traces(args.path):
+        if trace.requirement_id:
+            trace_el = ET.SubElement(
+                traces, "trace", requirement_id=trace.requirement_id
+            )
+        else:
+            trace_el = ET.SubElement(traces, "trace")
+        if trace.location.file:
+            line = str(trace.location.line)
+            file = trace.location.file
+            location = ET.SubElement(
+                trace_el,
+                "location",
+                line=line,
+                file=file,
+            )
+        else:
+            location = ET.SubElement(
+                trace_el,
+                "location",
+            )
+        name = trace.location.name
+        location.text = f"{name}"
+    tree = ET.ElementTree(traces)
+    with open("malkoha.xml", "wb") as f:
+        tree.write(f)
